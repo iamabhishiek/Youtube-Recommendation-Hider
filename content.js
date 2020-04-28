533e972f4c27ca6231dadc3d333126bacb8b6680
Abhishiek Mahajan <abhishiek.mahajan@nagarro.com>
2020-04-28 12:47:27 +0530

    First Commit

---
+++ content.js
@@ -0,0 +1,10 @@
+document.getElementById("contents").style.visibility = "hidden";
+document.getElementById("items").style.visibility = "hidden";
+document.getElementById("comments").style.visibility = "hidden";
+document.getElementById("related").style.visibility = "hidden";
+// document.getElementById("primery").style.backgroundImage = "url('img.jpg')";
+// var img = document.createElement("img");
+// img.src = "img.jpg";
+
+// document.getElementById("title") = "";
+// document.getElementById("title").appendChild(img);
